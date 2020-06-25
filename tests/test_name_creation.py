10029296abd0500f258a6104358427d46327480f
phibzy <phibzy@gmail.com>
2020-06-25 20:20:07 +1000

    Added more tests

--- tests/test_name_creation.py
+++ tests/test_name_creation.py
@@ -33,3 +33,68 @@ class TestNameCreation(unittest.TestCase):
         a = Board([Player('Bob'), Player('Bob')])
         self.assertEqual(a.players, [Player('Bob'), Player('Bob(1)')], "Fails basic case")
 
+    
+    def test_ranom_order_same_name(self):
+        """ Random order duplicate name case """
+        a = Board([
+                    Player('Bob'),
+                    Player('Mike'),
+                    Player('Tree'),
+                    Player('Mike'),
+                    Player('Willis'),
+                    Player('Bob')
+                  ])
+
+        self.assertEqual(a.players, [ 
+                    Player('Bob'),
+                    Player('Mike'),
+                    Player('Tree'),
+                    Player('Mike(1)'),
+                    Player('Willis'),
+                    Player('Bob(1)')
+                  ]
+        , "Fails random case")
+
+
+
+    def all_same_name_with_extras(self):
+        """ All same name, with some weird cases """
+        a = Board([
+                    Player('Bob'),
+                    Player('Bob'),
+                    Player('Bob(2)'),
+                    Player('bob'),
+                    Player('Bob'),
+                    Player('Bob(3)'),
+                    Player('Bob'),
+                  ])
+
+        self.assertEqual(a.players, [ 
+                    Player('Bob'),
+                    Player('Bob(1)'),
+                    Player('Bob(2)'),
+                    Player('bob'),
+                    Player('Bob(3)'),
+                    Player('Bob(3)(1)'),
+                    Player('Bob(4)'),
+                  ]
+        , "Fails random case")
+
+    def test_default_duplicate(self):
+        """ Single duplicate name case """
+        a = Board([
+                    Player(''),
+                    Player(''),
+                    Player(''),
+                    Player('Gary(2)'),
+                    Player(''),
+                  ])
+
+        self.assertEqual(a.players, [ 
+                    Player('Gary'),
+                    Player('Gary(1)'),
+                    Player('Gary(2)'),
+                    Player('Gary(2)(1)'),
+                    Player('Gary(3)'),
+                  ]
+        , "Fails random case")
--- tests/test_range_insert.py
+++ tests/test_range_insert.py
@@ -2,29 +2,91 @@
 
 import unittest
 from JustOneOfThoseDays.board import Board
-from JustOneOfThoseDays.player import Player
 from JustOneOfThoseDays.card import Card
+from JustOneOfThoseDays.hand import Hand
+from JustOneOfThoseDays.player import Player
+
 
 class testRangeInsert(unittest.TestCase):
     
     #TODO: Test inserting into ranges at the end (e.g. with 100)
 
 
-    def testFrontInsert(self):
+    def test_front_insert(self):
         b = Board([Player("Akerstache"), Player("Steven Wilton")])
-        testPlayer = b.players[b.current_starter]
+        test_player = b.players[b.current_starter]
         
-        self.assertEqual(len(testPlayer.hand.ranges), testPlayer.hand.num_cards + 1, "Error - Number of ranges invalid")
+        self.assertEqual(len(test_player.hand.ranges), test_player.hand.num_cards + 1, "Error - Number of ranges invalid")
 
-        newCard = Card("Nothing happens", 0)
+        new_card = Card("Nothing happens", 0)
 
-        testPlayer.hand.gain_card(newCard)
+        test_player.hand.gain_card(new_card)
 
-        self.assertEqual(len(testPlayer.hand.ranges), testPlayer.hand.num_cards + 1, "Error - Number of ranges invalid")
-        self.assertEqual(testPlayer.hand.cards[0], newCard, "Error - Card not inserted in front")
-        self.assertEqual(testPlayer.hand.ranges[0], (0,0), "Error - Range not 0,0")
-        self.assertEqual(testPlayer.hand.ranges[1][0], 0, "Error - First element of next tuple not changed")
-
-
+        self.assertEqual(len(test_player.hand.ranges), test_player.hand.num_cards + 1, "Error - Number of ranges invalid")
+        self.assertEqual(test_player.hand.cards[0], new_card, "Error - Card not inserted in front")
+        self.assertEqual(test_player.hand.ranges[0], (0,0), "Error - Range not 0,0")
+        self.assertEqual(test_player.hand.ranges[1][0], 0, "Error - First element of next tuple not changed")
 
 
+    def test_end_insert(self):
+        p = Player("Damo")
+        p.hand.gain_card(Card("Eat moldy cheese", 15))
+        p.hand.gain_card(Card("Swallow pen ink", 38))
+        p.hand.gain_card(Card("Hit in face with brick", 74))
+
+        self.assertEqual(p.hand.num_cards, 3, "num_cards error")
+        self.assertEqual(p.hand.ranges, [(0,15), (15,38), (38,74), (74, 100)], "End insert case 1")
+
+        p.hand.gain_card(Card("Cut in half by samurai warrior", 90))
+
+        self.assertEqual(p.hand.num_cards, 4, "num_cards error")
+        self.assertEqual(p.hand.ranges, [(0,15), (15,38), (38,74), (74,90), (90, 100)], "End insert case 2")
+
+        p.hand.gain_card(Card("Eaten", 100))
+
+        self.assertEqual(p.hand.num_cards, 5, "num_cards error")
+        self.assertEqual(p.hand.ranges, [(0,15), (15,38), (38,74), (74,90), (90, 100), (100, 100)], "End insert case 3")
+
+    def test_middle_insert(self): 
+        p = Player("Damo")
+        p.hand.gain_card(Card("Eat moldy cheese", 15))
+        p.hand.gain_card(Card("Swallow pen ink", 38))
+        p.hand.gain_card(Card("Hit in face with brick", 74))
+
+        self.assertEqual(p.hand.num_cards, 3, "num_cards error")
+        self.assertEqual(p.hand.ranges, [(0,15), (15,38), (38,74), (74, 100)], "Middle insert case 1")
+
+        p.hand.gain_card(Card("Flat tire", 42))
+        self.assertEqual(p.hand.ranges, [(0,15), (15,38), (38, 42), (42,74), (74, 100)], "Middle insert case 2")
+
+        p.hand.gain_card(Card("Writer's block", 40))
+        self.assertEqual(p.hand.ranges, [(0,15), (15,38), (38,40), (40, 42), (42,74), (74, 100)], "Middle insert case 3")
+        p.hand.gain_card(Card("Tester's block", 39))
+        self.assertEqual(p.hand.ranges, [(0,15), (15,38), (38,39), (39,40), (40, 42), (42,74), (74, 100)], "Middle insert case 3")
+
+    def test_duplicate_inserts(self):
+        p = Player("Test Dummy")
+        p.hand.gain_card(Card("Eat moldy cheese", 30))
+        p.hand.gain_card(Card("Swallow pen ink", 20))
+        p.hand.gain_card(Card("Hit in face with brick", 81))
+
+        self.assertEqual(p.hand.num_cards, 3, "num_cards error")
+        self.assertEqual(p.hand.ranges, [(0,20), (20,30), (30,81), (81, 100)], "Duplicate insert case 1")
+
+        p.hand.gain_card(Card("Bitten by rat", 30))
+        self.assertEqual(p.hand.ranges, [(0,20), (20,30), (30,30), (30,81), (81, 100)], "Duplicate insert case 2")
+
+        p.hand.gain_card(Card("Bitten by monkey", 30))
+        self.assertEqual(p.hand.ranges, [(0,20), (20,30), (30,30), (30,30), (30,81), (81, 100)], "Duplicate insert case 2")
+
+        p.hand.gain_card(Card("Devoured by giant spider", 81))
+        self.assertEqual(p.hand.ranges, [(0,20), (20,30), (30,30), (30,30), (30,81), (81,81), (81, 100)], "Duplicate insert case 3")
+
+        p.hand.gain_card(Card("Devoured by giant spider", 81))
+        self.assertEqual(p.hand.ranges, [(0,20), (20,30), (30,30), (30,30), (30,81), (81, 81), (81, 81), (81, 100)], "Duplicate insert case 4")
+
+        p.hand.gain_card(Card("Devoured by giant spider", 81))
+        self.assertEqual(p.hand.ranges, [(0,20), (20,30), (30,30), (30,30), (30,81), (81, 81), (81, 81), (81, 81), (81, 100)], "Duplicate insert case 5")
+
+        p.hand.gain_card(Card("Devoured by giant spider", 81))
+        self.assertEqual(p.hand.ranges, [(0,20), (20,30), (30,30), (30,30), (30,81), (81, 81), (81, 81), (81, 81), (81, 81), (81, 100)], "Duplicate insert case 6")
